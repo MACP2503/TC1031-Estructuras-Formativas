e37d67420e735c335c2a678596fd81043e6f5504
MACP2503 <74038341+MACP2503@users.noreply.github.com>
2021-08-25 17:44:18 -0500

    Header de las funciones
    Contiene las 3 funciones exigidas.

---
+++ Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
@@ -0,0 +1,15 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+
+class funciones
+{
+    public:
+        funciones();
+        ~funciones();
+        int sumaIterativa(int);
+        int sumaRecursiva(int);
+        int sumaDirecta(int);
+};
+
+#endif // FUNCIONES_H
