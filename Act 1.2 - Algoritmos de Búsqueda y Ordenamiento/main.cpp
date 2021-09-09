563c0a12bf4cf5d4f8d60d19656f043a9ff40d9f
MACP2503 <74038341+MACP2503@users.noreply.github.com>
2021-09-09 02:55:15 -0500

    Update main.cpp

--- Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/main.cpp
+++ Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/main.cpp
@@ -37,50 +37,50 @@ int main(int argc, char* argv[]) {
 	vector<int> original2 (array2, array2 + sizeof(array2) / sizeof(int) );
 
 	vector<int> prueba;
-	Sorts<int> sorts;
+	sorts sorts;
 	string resp1 = "[1, 3, 4, 5, 8, 11, 21, 28, 52, 54, 58, 61, 64, 65, 75, 78, 84, 91, 92, 92]";
 	string resp2 = "[5, 10, 68, 120, 333, 897]";
 
-	prueba = original1;
-	sorts.ordenaSeleccion(prueba);
-
+    prueba = original1;
+	prueba = sorts.ordenaSeleccion(prueba);
 
 	cout << "\n" <<"1.- esperada " << resp1 << "\n programa " << arrayToString(prueba) << "\n";
 	cout <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
-	/*prueba = original2;
-	sorts.ordenaSeleccion(prueba);
+	prueba = original2;
+	prueba = sorts.ordenaSeleccion(prueba);
 
 	cout << "\n" <<"2.- esperada " << resp2 << "\n programa " << arrayToString(prueba) << "\n";
 	cout <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
 	prueba = original1;
-	sorts.ordenaBurbuja(prueba);
+	prueba = sorts.ordenaBurbuja(prueba);
 
 	cout << "\n" <<"3.- esperada " << resp1 << "\n   programa " << arrayToString(prueba) << "\n";
 	cout <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
 	prueba = original2;
-	sorts.ordenaBurbuja(prueba);
+	prueba = sorts.ordenaBurbuja(prueba);
 
 	cout << "\n" <<"4.- esperada "  << resp2 << "\n programa " << arrayToString(prueba) << "\n";
 	cout <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
 	prueba = original1;
-	sorts.ordenaMerge(prueba);
+	prueba = sorts.ordenaMerge(prueba);
 
 	cout << "\n" <<"5.- esperada "  << resp1 << "\n programa " << arrayToString(prueba) << "\n";
 	cout <<	(!resp1.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
 	prueba = original2;
-	sorts.ordenaMerge(prueba);
+	prueba = sorts.ordenaMerge(prueba);
 	cout << "\n" <<"6.- esperada "  << resp2 << "\n programa " << arrayToString(prueba) << "\n";
 	cout <<	(!resp2.compare(arrayToString(prueba)) ? "success\n" : "fail\n");
 
-	sorts.ordenaMerge(prueba);
+	prueba = sorts.ordenaMerge(prueba);
+
 
 	cout << "\n" <<"7.- esperada "  << 5 << " programa " << sorts.busqSecuencial(prueba, 897) << "\n";
-  cout <<	(5 == sorts.busqSecuencial(prueba, 897) ? "success\n" : "fail\n");
+    cout <<	(5 == sorts.busqSecuencial(prueba, 897) ? "success\n" : "fail\n");
 
 	cout << "\n" <<"8.- esperada "  << -1  << " programa " << sorts.busqSecuencial(prueba, 100) << "\n";
 	cout <<	(-1 == sorts.busqSecuencial(prueba, 100) ? "success\n" : "fail\n");
@@ -89,5 +89,5 @@ int main(int argc, char* argv[]) {
 	cout <<	(4 == sorts.busqBinaria(prueba, 333) ? "success\n" : "fail\n");
 
 	cout << "\n" <<"10.- esperada "  << -1  << " programa " << sorts.busqBinaria(prueba, 197) << "\n";
-	cout <<	(-1 == sorts.busqBinaria(prueba, 197) ? "success\n" : "fail\n");*/
+	cout <<	(-1 == sorts.busqBinaria(prueba, 197) ? "success\n" : "fail\n");
 }
